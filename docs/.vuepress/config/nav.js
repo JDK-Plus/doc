465ac83baa542e8445bb9c9376f4d8e3a95e938d
pf <pf@jdk.plus>
2021-12-22 16:14:45 +0800

    去掉菜单

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,10 +1,5 @@
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '工程组件', link: '/',
-    items: [
-      { text: 'validator-plus', link: '/pages/13728c/' },
-    ]
-  },
   { text: 'JDK源码分析', link: '/pages/e3ab59/',
     // items: [
     //   { text: 'validator-plus', link: '/pages/13728c/' },
