f44043691b2ca38201563188603e1e0758e5f231
pf <pf@jdk.plus>
2021-12-31 15:51:19 +0800

    添加针对spring-boot-starter-broadcast的说明

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,6 +5,12 @@ module.exports = [
     //   { text: 'validator-plus', link: '/pages/13728c/' },
     // ]
   },
+  {
+    text: '公用组件', link: '/pages/815166/',
+    items: [
+      { text: 'spring-boot-starter-broadcast', link: '/pages/815166/' },
+    ]
+  },
   { text: '问答', link: '/pages/9cc27d/' },
   { text: '反馈', link: '/pages/5dfce5/' },
 ]
